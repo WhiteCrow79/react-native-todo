fe955d5415e1feae5f0fb0d9a8c461715348b441
whitecrow <whitecrow@whitecrowui-Macmini.local>
2022-07-28 00:07:47 +0900

    Input Component Add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'react-native';
 import styled, { ThemeProvider } from 'styled-components/native';
 import { theme } from './theme';
 import Input from './component/Input';
 
-const Container = styled.View`
+const Container = styled.SafeAreaView`
   flex: 1;
   background-color: ${({ theme }) => theme.background};
   align-items: center;
@@ -20,6 +20,16 @@ const Title = styled.Text`
 `;
 
 export default function App() {
+  const [newTask, setNewTask] = useState('');
+
+  const _addTask = () => {
+    alert(`Add: ${newTask}`);
+    setNewTask('');
+  };
+
+  const _handleTextChange = (text) => {
+    setNewTask(text);
+  };
   return (
     <ThemeProvider theme={theme}>
       <Container>
@@ -28,7 +38,12 @@ export default function App() {
           backgroundColor={theme.background}
         />
         <Title>TODO List</Title>
-        <Input />
+        <Input
+          placeholder="+ Add a Task"
+          value={newTask}
+          onChangeText={_handleTextChange}
+          onSubmitEditing={_addTask}
+        />
       </Container>
     </ThemeProvider>
   );
